90ccb9db41c070dda3d46490c80e69b8727551ec
Alfred Makne Poulsen <=>
2023-11-24 22:13:01 +0100

    gangmember movement

--- Scripts/building.gd
+++ Scripts/building.gd
@@ -3,13 +3,12 @@ var player = 1
 var spawn_timer = 0
 var spawn_time = 3
 var enemy_scene
-var spawn_radius = 50
+var spawn_radius = 70
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	enemy_scene = preload("res://scenes/gangmember.tscn")
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -22,7 +21,9 @@ func spawner():
 	if(player == 0): return
 	if(spawn_timer <= 0):
 		spawn_timer = spawn_time
-		var new_enemy = enemy_scene.instantiate()
-		var spawn_pos = Vector2(randf_range(-spawn_radius,spawn_radius),randf_range(-spawn_radius,spawn_radius))
-		new_enemy.position = position+spawn_pos
+		var new_enemy:Node2D = enemy_scene.instantiate()
+		var spawn_pos:Vector2 = position+Vector2(randf_range(-spawn_radius,spawn_radius),randf_range(-spawn_radius,spawn_radius))
+		new_enemy.position = position
+		new_enemy.building = self
+		new_enemy.target = spawn_pos
 		get_parent().add_child(new_enemy)
--- Scripts/gangmember.gd
+++ Scripts/gangmember.gd
@@ -1,14 +1,37 @@
 extends Node2D
-var target
-@export var movement_speed = 50
+var target:Vector2
+@export var movement_speed = 30
+var building
+enum mv_state {TARGET, IDLE}
+var state:mv_state = mv_state.TARGET
+var idle_radius:float = 10
+var idle_timer:float = 0
+var idle_time:float = 2
+var idle_target:Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	target = Vector2(200,200)
+	pass
 	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	match state:
+		mv_state.TARGET:
+			move(delta);
+		mv_state.IDLE:
+			idle(delta)
+			
+	idle_timer -= delta
+
+func move(delta):
 	position = position.move_toward(target, movement_speed*delta)
-	
+	if(position.x > target.x-0.5 && position.x < target.x+0.5 && position.y > target.y-0.5 && position.y < target.y+0.5):
+		state = mv_state.IDLE
+
+func idle(delta):
+	if(idle_timer < 0):
+		idle_timer = idle_time
+		idle_target = Vector2(randf_range(target.x-idle_radius,target.x+idle_radius),randf_range(target.y-idle_radius,target.y+idle_radius))
+	position = position.move_toward(idle_target,movement_speed*delta)
