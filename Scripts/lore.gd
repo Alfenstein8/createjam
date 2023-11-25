bb33b70272d4736c6f16795504e91b4c1dbcddde
Alfred Makne Poulsen <=>
2023-11-25 23:01:21 +0100

    Added GANG in lore

--- Scripts/lore.gd
+++ Scripts/lore.gd
@@ -1,5 +1,5 @@
 extends Control
-var main_menu = preload("res://scenes/main_menu.tscn")
+var main_menu = preload("res://scenes/level.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
